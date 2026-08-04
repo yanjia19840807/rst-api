f90fc7a90e05741f134e1afdd2e1917e7e849955
yanjia <yanjiafelix@gmail.com>
2026-08-04 15:44:49 +0800

    first commit

---
+++ src/main/java/com/cmacgm/gbs/rst/api/RstApiApplication.java
@@ -0,0 +1,13 @@
+package com.cmacgm.gbs.rst.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RstApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmacgm/gbs/rst/api/RstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmacgm.gbs.rst.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
